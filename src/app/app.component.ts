fba2c5e524fcc9dde19e705627a7cd7f318d93c4
Raoul DIFFOUO <diraulo@gmail.com>
2018-05-14 14:18:56 +0200

    Pass data to the contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   constructor() {
     this.initContact();
+    this.initListOfContacts();
   }
 
   createContact() {
@@ -29,5 +30,40 @@ export class AppComponent {
       location: '',
       notes: ''
     };
+  }
+
+  private initListOfContacts() {
+    this.contacts = [
+      {
+        name: 'Raoul Diffouo',
+        email: 'diraulo@craftacademy.se',
+        company: 'Craft Academy',
+        role: 'Coach',
+        twitter: 'diraulo',
+        location: 'Pretoria',
+        notes: 'Can be annoying sometimes? :thinking:',
+        image: 'https://avatars2.githubusercontent.com/u/4028374?s=40&v=4'
+      },
+      {
+        name: 'Magnus',
+        email: 'magnus@craftacademy.se',
+        company: 'Craft Academy',
+        role: 'Coach',
+        twitter: 'magnus',
+        location: 'Gothenburg',
+        notes: 'Awesome Guy',
+        image: ''
+      },
+      {
+        name: 'Faraz',
+        email: 'faraz@craftacademy.se',
+        company: 'Craft Academy',
+        role: 'Coach',
+        twitter: 'faraz',
+        location: 'Stockholm',
+        notes: 'FooBar',
+        image: ''
+      }
+    ];
   }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,20 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  contact: any;
+  @Input() contact: any;
 
-  constructor() {
-    this.contact = {
-      name: 'Raoul Diffouo',
-      email: 'diraulo@craftacademy.se',
-      company: 'Craft Academy',
-      role: 'Coach',
-      twitter: 'diraulo',
-      location: 'Pretoria',
-      notes: 'Can be annoying sometimes? :thinking:',
-      image: 'https://avatars2.githubusercontent.com/u/4028374?s=40&v=4'
-    };
-  }
+  constructor() { }
 
   ngOnInit() {
   }
